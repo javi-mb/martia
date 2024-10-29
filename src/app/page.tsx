26f7c3bfc7118a5d4a5e10404fbf2ee8f430e40e
Javi Martinez <martinezbjavier@hotmail.com>
2024-10-29 15:45:30 -0300

    fix logoticker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import { Banner } from "@/components/Banner";
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
-import { LogoTicker } from "@/components/LogoTicker";
+
 import { Features } from "@/components/Features";
 import { ProductShowcase } from "@/components/ProductShowcase";
 import { FAQs } from "@/components/FAQs";
